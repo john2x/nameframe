d172dc12ec7d195cf9bd1c369935f97e03154370
John Louis Del Rosario <john2x@gmail.com>
2015-10-02 11:27:29 +0800

    Prepare for MELPA

--- nameframe-perspective.el
+++ nameframe-perspective.el
@@ -2,8 +2,21 @@
 
 ;; Author: John Del Rosario <john2x@gmail.com>
 ;; URL: https://github.com/john2x/nameframe
-;; Version: 0.1.0-alpha
-;; Package-Requires: ((nameframe "0.1.0-alpha") (perspective "1.12"))
+;; Version: 0.2.0-beta
+;; Package-Requires: ((nameframe "0.2.0-beta") (perspective "1.12"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
--- nameframe-projectile.el
+++ nameframe-projectile.el
@@ -2,8 +2,21 @@
 
 ;; Author: John Del Rosario <john2x@gmail.com>
 ;; URL: https://github.com/john2x/nameframe
-;; Version: 0.1.0-alpha
-;; Package-Requires: ((nameframe "0.1.0-alpha") (projectile "0.13.0"))
+;; Version: 0.2.0-beta
+;; Package-Requires: ((nameframe "0.2.0-beta") (projectile "0.13.0"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
@@ -50,7 +63,6 @@ unless we're already in that frame."
 ;;;###autoload
 (defun nameframe-projectile-init ()
   "Override Projectile's mapping of `projectile-switch-project' to use `nameframe-projectile-switch-project'."
-  (interactive)
   (define-key projectile-mode-map [remap projectile-switch-project] 'nameframe-projectile-switch-project))
 
 (provide 'nameframe-projectile)
