a61aeaabef0d6837de743363421a706ae0a2f170
John Louis Del Rosario <john2x@gmail.com>
2022-10-24 12:03:42 +1300

    Require emacs 28.1

--- nameframe-project.el
+++ nameframe-project.el
@@ -3,7 +3,7 @@
 ;; Author: John Del Rosario <john2x@gmail.com>
 ;; URL: https://github.com/john2x/nameframe
 ;; Version: 0.5.0-beta
-;; Package-Requires: ((nameframe "0.5.0-beta") (project "0.8.1"))
+;; Package-Requires: ((emacs "28.1") (nameframe "0.5.0-beta") (project "0.8.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
